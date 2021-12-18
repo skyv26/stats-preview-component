80516c4b168ed3c60f500b94f0ef15652f0d750e
skyv26 <dev.aakashv@gmail.com>
2021-12-18 21:39:23 +0530

    footer updated

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import { InsightWrapper } from "./styles/TextContainer.styled";
 
 function App() {
   return (
-    <Main>
-      <Container>
-        <StatsImageContainer />
-        <Display>
-          <TextWrapper>
-            <TextContainer />
-          </TextWrapper>
-          <InsightWrapper>
-            <Stats />
-          </InsightWrapper>
-        </Display>
-      </Container>
-    </Main>
+    <>
+      <Main>
+        <Container>
+          <StatsImageContainer />
+          <Display>
+            <TextWrapper>
+              <TextContainer />
+            </TextWrapper>
+            <InsightWrapper>
+              <Stats />
+            </InsightWrapper>
+          </Display>
+        </Container>
+      </Main>
+      <footer class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62/hub/stats-preview-card-component-YhZgHX8Tf" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Made with ❤️ By <a href="https://www.frontendmentor.io/profile/skyv26" target="_blank" rel="noreferrer"><strong>Aakash Verma</strong></a>.
+      </footer>
+    </>
   );
 }
 

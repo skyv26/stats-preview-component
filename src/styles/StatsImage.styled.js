988c0dc5025617fac4f288d61a3d3db454162144
Aakash Verma <dev.aakashv@gmail.com>
2022-10-12 14:33:29 +0530

    Update `StatsImage.styled.js` file

--- src/styles/StatsImage.styled.js
+++ src/styles/StatsImage.styled.js
@@ -1,33 +1,32 @@
 import styled from "styled-components";
-import { Image, Wrapper } from './global.styled';
+import { Image, Wrapper } from "./global.styled";
 
 export const Img = styled(Image)({
-    width: '100%',
+  width: "100%",
 });
 
 export const ImageWrapper = styled(Wrapper)({
-    position: 'relative',
+  position: "relative",
 
+  ".desktop": {
+    display: "none",
+  },
+
+  "@media(min-width: 64em)": {
     ".desktop": {
-        display: 'none',
+      display: "block",
     },
-
-    "@media(min-width: 64em)": {
-        ".desktop": {
-            display: "block",
-        },
-        ".mobile": {
-            display: "none",
-        }
-    }
+    ".mobile": {
+      display: "none",
+    },
+  },
 });
 
 export const Overlay = styled(Wrapper)({
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    top: '0',
-    left: '0',
-    backgroundColor: 'hsla(277, 64%, 65%, 0.6)',
-
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: "0",
+  left: "0",
+  backgroundColor: "hsla(277, 64%, 65%, 0.6)",
 });

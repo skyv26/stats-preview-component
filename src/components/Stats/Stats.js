3983c5efb3da1db31e3079253c7506038e42c9a0
Aakash Verma <dev.aakashv@gmail.com>
2022-10-17 14:38:23 +0530

    Update `Stats.js` file

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { InsightPara, InsightSpan } from '../../styles/TextContainer.styled';
+import React from "react";
+import { InsightPara, InsightSpan } from "../../styles/TextContainer.styled";
 
 const Stats = (props) => {
-    return (
-        <>
-            <InsightPara>
-                10k+
-                <InsightSpan>companies</InsightSpan>
-            </InsightPara>
+  return (
+    <>
+      <InsightPara>
+        10k+
+        <InsightSpan>companies</InsightSpan>
+      </InsightPara>
 
-            <InsightPara>
-                314
-                <InsightSpan>templates</InsightSpan>
-            </InsightPara>
+      <InsightPara>
+        314
+        <InsightSpan>templates</InsightSpan>
+      </InsightPara>
 
-            <InsightPara>
-                12m+
-                <InsightSpan>queries</InsightSpan>
-            </InsightPara>
-        </>
-    );
-}
+      <InsightPara>
+        12m+
+        <InsightSpan>queries</InsightSpan>
+      </InsightPara>
+    </>
+  );
+};
 
 export default Stats;
